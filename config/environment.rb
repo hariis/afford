7aa33774ad46f283b9807da03d40534902733869
hariis <hrajagopal@yahoo.com>
2010-10-18 21:20:24 -0700

    rails ver

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 #if ENV['RAILS_ENV'] == 'production'
-#RAILS_GEM_VERSION = '2.3.9' unless defined? RAILS_GEM_VERSION 
+RAILS_GEM_VERSION = '2.3.9' unless defined? RAILS_GEM_VERSION 
 #end
 #if ENV['RAILS_ENV'] != 'production'
 #RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION 
