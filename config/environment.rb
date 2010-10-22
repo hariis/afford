62f9d9714c114f893aba2309624b68ee35f9e718
hariis <hrajagopal@yahoo.com>
2010-10-21 23:28:26 -0700

    env

--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,9 @@
 if ENV['RAILS_ENV'] == 'production'
   RAILS_GEM_VERSION = '2.3.10' unless defined? RAILS_GEM_VERSION
 end
-if ENV['RAILS_ENV'] != 'production'
-  RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
-end
+#if ENV['RAILS_ENV'] != 'production'
+#  RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+#end
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
