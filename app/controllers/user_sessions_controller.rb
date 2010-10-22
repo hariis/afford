7406b701c4365a7587eb2edceb69a499a233b0cb
hariis <hrajagopal@yahoo.com>
2010-10-22 13:48:53 -0700

    removed 404

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,10 +1,10 @@
 class UserSessionsController < ApplicationController
 
-   before_filter :redirect_to_error, :except => [:new, :create, :destroy]
-  
-   def redirect_to_error
-      render 'questions/404', :status => 404, :layout => false and return
-   end
+#   before_filter :redirect_to_error, :except => [:new, :create, :destroy]
+#
+#   def redirect_to_error
+#      render 'questions/404', :status => 404, :layout => false and return
+#   end
      
   def new
     store_location
