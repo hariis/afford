d97bc2747053ae40a09328d2fd78763578576249
hariis <hrajagopal@yahoo.com>
2010-10-30 23:29:23 -0700

    todos

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -18,3 +18,22 @@
       observe('mouseout', toggle.curry(false));
   });
 })();
+function setFocus(){
+ var flag=false;
+ for(z=0;z<document.forms.length;z++){
+  var form = document.forms[z];
+  var elements = form.elements;
+  for (var i=0;i<elements.length;i++){
+    var element = elements[i];
+    if(element.type == 'text' &&
+      !element.readOnly &&
+      !element.disabled &&
+       element.id != 'subscriber_email'){
+      element.focus();
+	  flag=true;
+     break;
+    }
+  }
+  if(flag)break;
+ }
+}
