70768dea5ba8d73c7db2c7def598689ea746d0d3
satishs <satish.fnu@gmail.com>
2010-11-03 23:10:38 -0700

    ui fixes

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -39,7 +39,12 @@ class QuestionsController < ApplicationController
   # GET /questions
   # GET /questions.xml
   def index
-    @questions = Question.find(:all, :order => 'created_at DESC')
+    #if current_user && current_user.username == 'admin'
+      @questions = Question.find(:all, :order => 'created_at DESC')
+    #else
+    #  @questions = Question.find(:all, :conditions => ['nick_name != ? && expert_details != ?', "nickname", nil], :order => 'created_at DESC')
+      #@questions = Question.find(:all, :conditions => ['expert_details = ?', 'null'], :order => 'created_at DESC')
+    #end
 
     respond_to do |format|
       format.html # index.html.erb
