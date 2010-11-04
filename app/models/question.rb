8f5b45c8bb3f72c53adeccf5616d6df411e11794
hariis <hrajagopal@yahoo.com>
2010-11-04 00:19:13 -0700

    report and to_currency

--- app/models/question.rb
+++ app/models/question.rb
@@ -236,24 +236,19 @@ class Question < ActiveRecord::Base
   
   #---------------------------------------------------------------------------------------------------------
   def check_rule1_income_expenses
-    #Is Net Income > Expenses + Recurring Expenses + Recurrring Loan Payment for item
+    #Is Net Income > Expenses + Recurring Expenses + Recurring Loan Payment for item
     diff = financial.net_income - addon_total_expenses
-    if diff >= 0
-      #@expert_details << "Your <b>Net Income</b> is good to match the <b>Total Expenses</b><br/>"
-      @expert_details << "<li class='green'>Your Total monthly expenses will be $#{addon_total_expenses} after the purchase"
-      @expert_details << " which will still be within your Net income.</li>"
+    if diff >= 0      
+      @expert_details << "<li class='green'>Your Total monthly expenses will be #{addon_total_expenses.to_currency} after the purchase."
+      @expert_details << " It will still be within your Net Income.</li>"
     else
       @expert_verdict = false
       includes = []
       includes << "recurring cost of the item " if self.recurring_item_cost > 0
       includes << "recurring loan payment for the item " if self.pm_financing_amount > 0
       include_string = includes.size > 0 ? " including " + includes.to_sentence : ""
-      @expert_details << "<li class='red'>Your Total monthly expenses #{include_string}  will be $#{addon_total_expenses} after the purchase"
-      @expert_details << " which will exceed your Net income by $#{diff * -1}.</li>"
-      #@expert_details << "<span class='red'>Your <b>Net Income</b> is/will be less than Total Expenses "
-      #@expert_details << "+ Recurring cost of the item " if self.recurring_item_cost > 0
-      #@expert_details << "+ Recurring loan payment for the item " if self.pm_financing_amount > 0
-      #@expert_details << "to support this purchase</span><br/>"
+      @expert_details << "<li class='red'>Your Total monthly expenses #{include_string}  will be #{addon_total_expenses.to_currency} after the purchase."
+      @expert_details << " It will exceed your Net Income by #{(diff * -1).to_currency}.</li>"
     end
   end
   
@@ -262,21 +257,21 @@ class Question < ActiveRecord::Base
     #add :monthly_cc_payments_at_zero to total loan payments
     if financial.cc_debt_gt_zero <= 0
       if financial.cc_debt_at_zero <= 0
-        @expert_details << "<li class='green'>Your have no <b>Credit card debt.</b></li>"
+        @expert_details << "<li class='green'>You have no <b>Credit card debt.</b></li>"
       else
-        @expert_details << "<li class='green'>Your have $#{financial.cc_debt_at_zero} <b>Credit card debt</b> @ 0 % interest.<br/>"
+        @expert_details << "<li class='green'>You have #{financial.cc_debt_at_zero.to_currency} in <b>Credit card debt</b> @ 0 % interest rate.</li>"
         if financial.monthly_cc_payments_at_zero > 0
-          @expert_details << "You are already making monthly payments on this.</li>"
+          @expert_details << "<li class='expert-notes'>Expert Notes: You are already making monthly payments on this.<br/>"
         else
-          @expert_details << "You are not yet making monthly payments on this. <br/>
-                          Expert suggests: Start paying off your <b>Credit card debt</b> first.<br/>"
+          @expert_details << "<li class='expert-notes'>Expert Notes: You are not yet making monthly payments on this.
+                          Start paying off your <b>Credit card debt</b> first.<br/>"
         end
         #Add a comment as to how long it will take to pay off outright from savings
         monthly_savings = financial.net_income - addon_total_expenses
         if monthly_savings > 0
           months_to_cover = financial.cc_debt_at_zero > monthly_savings ?
                         "approximately " + (financial.cc_debt_at_zero.to_f / monthly_savings.to_f).to_s + " months" : "less than a month"
-          @expert_details << "Note: At your current monthly savings of $#{monthly_savings}, it will take #{months_to_cover} to pay off the debt outright.</li>"
+          @expert_details << "At your current monthly savings of #{monthly_savings.to_currency}, it will take #{months_to_cover} to pay off the debt outright.</li>"
         else
           @expert_details << "</li>"
         end
@@ -284,8 +279,8 @@ class Question < ActiveRecord::Base
       end
     else
       @expert_verdict = false
-      @expert_details << "<li class='red'>You have $#{financial.cc_debt_gt_zero} <b>Credit card debt</b> @ more than 0 % interest.<br/>
-                          Expert suggests: Pay off your <b>Credit card debt</b> first.</li>"
+      @expert_details << "<li class='red'>You have #{financial.cc_debt_gt_zero.to_currency} in <b>Credit card debt</b> @ more than 0 % interest rate.</li>
+                          <li class='expert-notes'>Expert Notes: Pay off your <b>Credit card debt</b> first.</li>"
     end
     
   end
@@ -298,18 +293,18 @@ class Question < ActiveRecord::Base
       move_funds = (6 * addon_total_expenses) - addon_liquid_assets
       if (addon_investment >= (8 * addon_total_expenses) - addon_liquid_assets)
         if self.reason_to_buy == 1 || self.reason_to_buy == 2
-          @expert_details << "<li class='green'>You don't have 6 times your total monthly expenses in <b>Liquid assets / Savings</b> for your emergency fund but you do have some <b>Investments. </b><br/>"
-          @expert_details << "Since you said that you deserve it or need it, you can first secure your emergency fund by liquidating $#{move_funds} from your <b>Investments</b> and moving it to <b>Savings</b> and then make the purchase.</li>"
+          @expert_details << "<li class='green'>You don't have 6 times your Total Monthly expenses in <b>Liquid Assets / Savings</b> for your emergency fund but you do have some <b>Investments. </b><br/>"
+          @expert_details << "Since you said that you deserve it or need it, you can first secure your emergency fund by liquidating #{move_funds.to_currency} from your <b>Investments</b> and moving it to <b>Savings</b> and then make the purchase.</li>"
         else
           @expert_verdict = false
-          @expert_details << "<li class='red'>Your $#{addon_liquid_assets} <b>Liquid assets / Savings</b> remaining after this purchase is not sufficient to cover for your emergency fund. <br/>Recommended is 6 times your total monthly expenses.</li>"
+          @expert_details << "<li class='red'>Your #{addon_liquid_assets.to_currency} <b>Liquid Assets / Savings</b> remaining after this purchase is not sufficient to cover for your emergency fund. <br/>Recommended is 6 times your Total Monthly expenses.</li>"
         end
       else
           @expert_verdict = false
-          @expert_details << "<li class='red'>Your $#{addon_liquid_assets} <b>Liquid assets / Savings</b> remaining after this purchase is not sufficient to cover for your emergency fund. <br/>Recommended is 6 times your total monthly expenses.</li>"
+          @expert_details << "<li class='red'>Your #{addon_liquid_assets.to_currency} <b>Liquid Assets / Savings</b> remaining after this purchase is not sufficient to cover for your emergency fund. <br/>Recommended is 6 times your Total Monthly expenses.</li>"
       end
     else
-      @expert_details << "<li class='green'>You have adequate <b>Liquid assets / Savings</b> for your emergency fund.</li>"
+      @expert_details << "<li class='green'>You have adequate <b>Liquid Assets / Savings</b> for your emergency fund.</li>"
     end
   end
   
@@ -330,20 +325,18 @@ class Question < ActiveRecord::Base
     rcontribution = 0.08*financial.net_income if self.age <= 40
     rcontribution = 0.10*financial.net_income if self.age > 40
     rdiff = financial.monthly_retirement_contribution - rcontribution
-    
-    #if (financial.monthly_retirement_contribution >= rcontribution)
+      
     if (rdiff >= 0)
-      @expert_details << "<li class='green'>Your $#{financial.monthly_retirement_contribution} monthly <b>retirement contribution</b> is good.</li>"
-    else
-      #@expert_details << "<li class='red'>Based on your age & income, $#{financial.monthly_retirement_contribution} monthly <b>retirement contribution</b> is less by $#{(rcontribution-financial.monthly_retirement_contribution).to_i}</li>"
+      @expert_details << "<li class='green'>Your #{financial.monthly_retirement_contribution.to_currency} monthly <b>retirement contribution</b> is good.</li>"
+    else      
       if self.age <= 40
-        @expert_details << "<li class='red'>Based on your age, $#{financial.monthly_retirement_contribution} monthly <b>retirement contribution</b> is less than 8% of your net income.</li>"
+        @expert_details << "<li class='red'>Based on your age, your monthly <b>retirement contribution</b> of #{financial.monthly_retirement_contribution.to_currency} is less than 8% of your net income.</li>"
       else
-        @expert_details << "<li class='red'>Based on your age, $#{financial.monthly_retirement_contribution} monthly <b>retirement contribution</b> is less than 10% of your net income.</li>"
+        @expert_details << "<li class='red'>Based on your age, your monthly <b>retirement contribution</b> of #{financial.monthly_retirement_contribution.to_currency} is less than 10% of your net income.</li>"
       end
       @expert_verdict = false
       cost = regular_deposit_in_future(rdiff.abs, 65-self.age)
-      @expert_details << "<li class='red'>Expert suggests: You are $#{rdiff.abs.to_i} behind your monthly <b>retirement contribution</b>. Being behind by $#{rdiff.abs.to_i} each month you will reduce your retirement nest by $#{cost}.</li>"
+      @expert_details << "<li class='expert-notes'>Expert Notes: You are #{rdiff.abs.to_i.to_currency} behind in your monthly <b>retirement contributions</b>. Note that this will lower your retirement nest egg by #{cost.to_currency} at age 65.</li>"
     end   
   end
   
@@ -364,15 +357,15 @@ class Question < ActiveRecord::Base
   def check_rule5_total_loan_payment
     #Total loan payment + Recurring Loan Payment for item < 36% of Gross monthly income. (+- 4%)
     if addon_total_loan_payment <= 0.36 * financial.gross_income
-       @expert_details << "<li class='green'>Your $#{addon_total_loan_payment} <b>Total Loan Payments</b> are less than or equal to 36% of your Gross income.</li>"
+       @expert_details << "<li class='green'>Your <b>Total Loan Payments</b> of #{addon_total_loan_payment.to_currency} are less than or equal to 36% of your Gross income.</li>"
     elsif addon_total_loan_payment <= 0.40 * financial.gross_income
         #loan_payment = (addon_total_loan_payment/financial.gross_income)*100 - 36 
         gap = addon_total_loan_payment - (0.36 * financial.gross_income)
-        @expert_details << "<li class='green'>Your $#{addon_total_loan_payment}<b> Total Loan Payments</b> are slightly greater than 36% of your Gross income.<br/>
-                          Expert suggests: You can still buy this item if you can reduce your total monthly loan payments by $#{gap.to_i}</li>"
+        @expert_details << "<li class='green'>Your #{addon_total_loan_payment.to_currency}<b> Total Loan Payments</b> are slightly greater than 36% of your Gross income.</li>
+                          <li class='expert-notes'>Expert Notes: You can still buy this item if you can reduce your Total Monthly loan payments by #{gap.to_i.to_currency}</li>"
     else
         @expert_verdict = false
-        @expert_details << "<li class='red'>Your $#{addon_total_loan_payment} <b>Total Loan Payments</b> are greater than 36% of your Gross Income.</li>"
+        @expert_details << "<li class='red'>Your #{addon_total_loan_payment.to_currency} <b>Total Loan Payments</b> are greater than 36% of your Gross Income.</li>"
         return false
     end
   end
@@ -380,27 +373,31 @@ class Question < ActiveRecord::Base
   def check_rule6_deferred_loan
     #if Deferred loans > 0, item cost < 1000
     if financial.deferred_loan_amount <= 0     
-      @expert_details << "<li class='green'>Your have no <b>Deferred loans</b> which is good.</li>"
-    elsif @expert_verdict == true #you are clean. no deniel from the previous rule
-      @expert_details << "<li class='green'>You have some deferred loans but you are doing well with your finances. Make sure to pay off your loan sooner than later.</li>"
-    elsif @expert_verdict == false #you are are mess. few deniels from the previous rule
+      @expert_details << "<li class='green'>Your have no <b>Deferred loans</b>.</li>"
+    elsif @expert_verdict == true #you are clean. no denial from the previous rules
+      @expert_details << "<li class='green'>You have some Deferred loans but you are doing well otherwise with your finances. Make sure to pay off your loan sooner than later.</li>"
+    elsif @expert_verdict == false #you are in a mess. few denials from the previous rules
       @expert_verdict = false
-      @expert_details << "<li class='red'>You have <b>Deferred loans</b> in the amount of $#{financial.deferred_loan_amount}.<br/>
-                        Expert suggests: Start paying off your Deferred loans first. This will save you money in the long run.</li>"
+      @expert_details << "<li class='red'>You have <b>Deferred loans</b> in the amount of #{financial.deferred_loan_amount.to_currency}.</li>
+                        <li class='expert-notes'>Expert Notes: Start paying off your loans. This will save you money in the long run.</li>"
     end
   end
   
  def check_rule7_item_cost_at_retirement
     if self.age < 55 #todo check if this is ok
-       cost = compound_interest(self.item_cost, 65-self.age)
-       #x = app_number_to_currency(cost)
-       @expert_details << "<hr/>Expert suggests: The $#{self.item_cost} item purchased now will be equivalent to $#{cost} at the age of 65. Please be sure that you really want to do this"
+       final_cost = compound_interest(self.item_cost, 65-self.age)
+       @expert_details << "<li class='expert-tips'>Expert Tips: #{(self.item_cost.to_currency)} invested now at 8% yearly returns would be worth #{final_cost.to_currency} at the age of 65."
     end
   end
-  
-  def  app_number_to_currency(value)
-     number_to_currency(value, :precision => 0)
-  end
-  
+
   #-------------------------------------------------------------------------------
+end
+
+require 'rubygems'
+require 'action_view'
+
+class Fixnum
+  def to_currency(options = {:precision => 0})
+    ActionView::Base.new.number_to_currency(self, options)
+  end
 end
