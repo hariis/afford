41958464c37f9587c5950553c83489cf91b45082
satishs <satish.fnu@gmail.com>
2010-11-12 21:04:50 -0800

    feedback back

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,14 +22,12 @@ class ApplicationController < ActionController::Base
   end
 
   def feedback
-      store_location
       experience = Experience.new
       experience.feedback_type = Experience::COMMENT_TYPES.index(params[:feedback_type].to_i) if params[:feedback_type]
       experience.description = params[:description]
       experience.email = params[:email] if params[:email]
       Notifier.deliver_send_experience(experience)
-      #render :nothing => true
-      redirect_back_or_default(root_path)
+      render :nothing => true
   end
    
   private 
